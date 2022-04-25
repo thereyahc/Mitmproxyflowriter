b99c9b31830dd636090b09a46b50137a64b31d1b
damedolla <glrahmtt@gmail.com>
2022-04-25 11:46:21 +0300

    Add files via upload

---
+++ flowriter.py
@@ -0,0 +1,54 @@
+from urllib import response
+from mitmproxy import http
+from mitmproxy import ctx
+import os
+import re
+import urllib.request
+from urllib.parse import urlparse
+from pathlib import Path
+import hashlib
+import pprint
+
+class parsetraffic:
+
+    def response(self, flow: http.HTTPFlow) -> None:
+        pp = pprint.PrettyPrinter(indent=4)
+        secfile = open("trafficsections.json","a")
+        secfile.write(pp.pprint(flow.get_state()))
+        if isinstance(flow, http.HTTPFlow):
+            if flow.response.headers["Content-Type"].find("application/octet-stream") != -1:
+                url = flow.request.pretty_url
+                filename = urlparse(url)
+                filename.path
+                filename2 = Path(filename.path).name
+                with open(filename2 ,"wb") as f:
+                    pageText = bytes(flow.response.content)
+                    f.write(bytes(pageText))
+                self.hashcal(filename2)
+            if flow.response.headers["Content-Type"].find("application/msword") != -1:
+                url = flow.request.pretty_url
+                filename = urlparse(url)
+                filename.path
+                filename2 = Path(filename.path).name
+                with open(filename2,"wb") as f:
+                    pageText = flow.response.content
+                    f.write(bytes(pageText))
+                self.hashcal(filename2)
+            if flow.response.headers["Content-Type"].find("application/pdf") != -1:
+                url = flow.request.pretty_url
+                filename = urlparse(url)
+                filename.path
+                filename2 = Path(filename.path).name
+                with open(filename2 ,"wb") as f:
+                    pageText = flow.response.content
+                    f.write(bytes(pageText))
+                self.hashcal(filename2)
+    def hashcal(self,fname):
+        with open(fname, "rb") as f:
+            file_hash = hashlib.md5()
+            while chunk := f.read():
+                file_hash.update(chunk)
+            hf = open("hashes.txt","a")
+            hf.writelines("{} : {} \n".format(fname,file_hash.hexdigest()))
+
+addons = [parsetraffic()]
